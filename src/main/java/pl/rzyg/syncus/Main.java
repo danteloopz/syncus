2c82b6948915dfbe0925dabc2c0c3ab7a05b7458
Dominik Burnóg <dominik.burnog@gmail.com>
2024-12-17 09:12:29 +0100

    started working on file tracker class

--- src/main/java/pl/rzyg/syncus/Main.java
+++ src/main/java/pl/rzyg/syncus/Main.java
@@ -7,13 +7,15 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        String OS = getOSVersion();
 
         if (args.length == 0) {
             //this will run the gui
+            logger.info("gui");
         } else {
             switch (args[0].toUpperCase()) {
                 case "-D":
-                    //this will run the daemon
+                    //this will run the daemon in the future
                     break;
                 case "-H":
                     //this will print the help
@@ -21,6 +23,7 @@ public class Main {
                     break;
                 case "-A":
                     //this will be the option to add a tracked directory/file
+                    fileTracker.
                     break;
                 case "-R":
                     //this will be to remove tracked directory/file
---
+++ src/main/java/pl/rzyg/syncus/fileTracker.java
@@ -0,0 +1,38 @@
+package pl.rzyg.syncus;
+
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.logging.LogManager;
+
+public class fileTracker {
+    jsonHandler json = new jsonHandler();
+
+
+    public boolean addFile(String[] loc, String OS, Logger logger) {
+        ArrayList<String[]> trackedFiles;
+        boolean success = false;
+        switch (OS){
+            case "WINDOWS":
+                json.winConfig();
+                trackedFiles = json.config.getFiles();
+                trackedFiles.add(loc);
+                json.config.setFiles(trackedFiles);
+                success = json.writeConfig(OS);
+                break;
+            case "LINUX":
+                json.linConfig();
+                trackedFiles = json.config.getFiles();
+                trackedFiles.add(loc);
+                success = json.writeConfig(OS);
+                break;
+            default:
+                logger.error("OS type not recognized");
+                System.out.println("Error: could not recognize os type");
+        }
+        if (!succes) {
+            logger.error("couldn't save config file");
+        }
+        return success;
+    }
+}
